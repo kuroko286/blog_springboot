d6cf56b40254785ba0f31678521704bb2e57d88a
Lê Văn Quốc <94890892+kuroko286@users.noreply.github.com>
2023-09-07 22:21:17 +0700

    first commit blog app using spring boot

---
+++ src/main/java/com/kuroko/blog/service/PostService.java
@@ -0,0 +1,17 @@
+package com.kuroko.blog.service;
+
+import com.kuroko.blog.payload.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
